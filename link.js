b8d6913d48dbc593a63cd63130f6c1fa3b4aaab2
Toaorukung <88958000+Toaorukung@users.noreply.github.com>
2025-07-05 07:04:04 +0700

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const scriptUrl = "https://script.google.com/macros/s/AKfycbw5L4DFDXAt52S13HgwpPBd3vu0MUKSG4Jfd0wsMDc9chaT2R3G9x-H4qwXqfrEX71D/exec"
+const scriptUrl = "https://script.google.com/macros/s/AKfycbyuRlOTs5eAg3QrVfmcQ-O8LSDiXnXqNqz1qEr6QawU8UTD_dLxfuRbVhgNr2d2U58u/exec"
